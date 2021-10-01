4fee2ab4095c7b9953a1ba4e294a735131b31daa
Greninja028 <guy.nithi@gmail.com>
2021-10-01 19:59:22 +0700

    Commit

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 import pygame, sys
+from settings import *
+from overworld import Overworld
+from level import Level
+
+class Game:
+    def __init__(self):
+        self.max_level = 2
+        self.overworld = Overworld(1, self.max_level, screen,self.create_level)
+        self.status = 'overworld'
+
+    def create_level(self,current_level):
+        self.level = Level(current_level,screen)
+        self.status = 'level'
+
+    def run(self):
+        if self.status == 'overworld':
+            self.overworld.run()
+        else:
+            self.level.run()
+
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+game = Game()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill('black')
+    game.run()
+
+    pygame.display.update()
+    clock.tick(60)
